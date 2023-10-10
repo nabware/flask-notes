8a25b148da8b8b089614756ee085a0ff47932418
Nabeel Rana <nabwaregroup@gmail.com>
2023-10-10 12:35:27 -0700

    work in progress

--- app.py
+++ app.py
@@ -31,8 +31,18 @@ def home():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Returns register form or handles register form submit"""
+
     form = RegisterUserForm()
 
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
 
+        user = User.register(username=username, password=password)
 
-    return render_template("index.html")
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(f"/users/{user.username}")
+
+    return render_template("register.html", form=form)
